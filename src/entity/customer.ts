cc95b4e99e56cf575a41934ed154d3178b51235d
Luis Azevedo <luis.azevedo@mercadobitcoin.com.br>
2022-03-27 15:58:44 -0300

    Entendendo aggregados

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,46 +1,51 @@
-class Customer{
+import Address from "./address";
+export default class Customer{
     _id: string; 
     _name: string = "";
-    _address: string = "";
+    _address!: Address;
     _active: boolean = true;
 
-    constructor(id: string){ //, name: string, address: string){
+    constructor(id: string, name: string){
         this._id = id;
-        // this._name = name;
-        // this._address = address;
+        this._name = name;
+        this.validate()
     }
 
-    set name(name:string){
-        this._name = name;
+    validate(){
+        if (this._name.length == 0){
+            throw new Error("Name is required")
+        }
+
+        if (this._id.length == 0){
+            throw new Error("Id is required")
+        }
     }
 
-    changeName(name: string){ // Aqui poderia ter uma regra de negócio, por exemplo, que ao informar o nome
-        // é obrigado também informar o sobrenome. Uma regra de negócio explicita.
+    changeName(name: string){
         this._name = name;
+        this.validate();
     }
 
     activate(){
+        if (this._address == undefined){
+            throw new Error("Address is mandatory to activate a customer.")
+        }
         this._active = true;
     }
 
     deactivate(){
         this._active = false;
     }
+
+    defineAddress(address: Address){
+        this._address = address;
+    }
+
+
+    setAddress(address: Address){
+        this._address = address;
+    }
 }
 
 
-
-let customer = new Customer("123");
-/* Isso aqui é errado, porque está definido apenas o ID. Eu preciso garantir que toda 
-vez que consultar os dados da entidade eles estão consistentes. Existe um cliente sem 
-nome na regra de negócio? Portanto essa classe entidade inconsistente. Uma vez que ela não 
-está consistente, ela não consegue validar regras de negócio, porque ela não serve para nada, 
-é um objeto incompleto. Em DDD eu preciso confiar 100 da vezes que os campos obrigatórios 
-estão preenchidos corretamente. 
-
-Vamos imaginar que esses dados vieram do banco de dados e ele não tem endereço, tudo bem, 
-não tem problema se a regra de negócio permite. Mas, uma vez que eu setei esse endereço, 
-no programa inteiro esse endereço preciso existir, se ela está persistido no banco de dados 
-ou não é um outro problema. Nesse momento é preciso esquecer de banco de dados, inclusive do ID, 
-porque esse ID identifica a entidade no meu sistema e não no banco de dados. 
-*/
+let customer = new Customer("123", "");
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import Address from './entity/address';
+import Customer from './entity/customer'
+import Order from './entity/order';
+import OrderItem from './entity/order_item';
+let customer = new Customer("123", "Luis Azevedo");
+const address = new Address("Rua dois", 2, "85580-000", "Itapejara");
+customer.defineAddress(address)
+customer.activate()
+
+const item1 = new OrderItem("1", "Item 1", 10);
+const item2 = new OrderItem("1", "Item 1", 10);
+
+const order = new Order("1", customer._id, [item1, item2])
